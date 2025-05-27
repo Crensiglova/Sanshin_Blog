cb1aab35fcc037a7a712145fed880fc5b1b4730c
Guilherme Crensiglova <51062332+Crensiglova@users.noreply.github.com>
2025-05-27 10:24:23 -0300

    Add files via upload
    Commit inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+// Futuras interações como carregamento de novas partituras ou comentários
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Sanshin Blog carregado!");
+    // Você pode carregar dinamicamente partituras usando fetch aqui, se quiser.
+  });
+  
